0207827152dcd9769b447d21f30ee2afc977ae57
Jakub <zkuba94@gmail.com>
2023-01-10 15:28:29 +0100

    task 2 new way in progress, last functionality lasts

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,133 +54,47 @@ const sort3 = arr => {
 };
 // sort3(array4);
 
-// Zadanie 2
-const array5 = ['MM', 'CXV', 'DCDXVI', 56];
-let convertedNumbers = [];
-let convertedRomanianNumber;
-let convertedArabicNumber;
+// ZADANIE 2
+// const array5 = [9, 15, 68, 120, 677, 1340, 2789];
+const array5 = ['LXXX', 'VIII', 'LXXXI', 'DLXXXVII'];
 const romanianNumbers = [
 	{
 		symbol: 'I',
 		value: 1,
 	},
 	{
-		symbol: 'II',
-		value: 2,
-	},
-	{
-		symbol: 'III',
-		value: 3,
-	},
-	{
-		symbol: 'IV',
-		value: 4,
-	},
-	{
 		symbol: 'V',
 		value: 5,
-	},
-	{
-		symbol: 'VI',
-		value: 6,
-	},
-	{
-		symbol: 'VII',
-		value: 7,
-	},
-	{
-		symbol: 'VIII',
-		value: 8,
-	},
-	{
-		symbol: 'IX',
-		value: 9,
 	},
 	{
 		symbol: 'X',
 		value: 10,
 	},
 	{
-		symbol: 'XX',
-		value: 20,
-	},
-	{
-		symbol: 'XXX',
-		value: 30,
-	},
-	{
-		symbol: 'XL',
-		value: 40,
-	},
-	{
 		symbol: 'L',
 		value: 50,
-	},
-	{
-		symbol: 'LX',
-		value: 60,
-	},
-	{
-		symbol: 'LXX',
-		value: 70,
-	},
-	{
-		symbol: 'LXXX',
-		value: 80,
-	},
-	{
-		symbol: 'XC',
-		value: 90,
 	},
 	{
 		symbol: 'C',
 		value: 100,
 	},
 	{
-		symbol: 'CC',
-		value: 200,
-	},
-	{
-		symbol: 'CCC',
-		value: 300,
-	},
-	{
-		symbol: 'CD',
-		value: 400,
-	},
-	{
 		symbol: 'D',
 		value: 500,
-	},
-	{
-		symbol: 'DC',
-		value: 600,
-	},
-	{
-		symbol: 'DCC',
-		value: 700,
-	},
-	{
-		symbol: 'DCCC',
-		value: 800,
-	},
-	{
-		symbol: 'CM',
-		value: 900,
 	},
 	{
 		symbol: 'M',
 		value: 1000,
 	},
-	{
-		symbol: 'MM',
-		value: 2000,
-	},
-	{
-		symbol: 'MMM',
-		value: 3000,
-	},
 ];
+let convertedNumbers = [];
+let units = [];
+let tens = [];
+let hundreds = [];
+let thousands = [];
+let convertedRomanianNumber = '';
+let romanianValue = [];
+let convertedArabicNumber = 0;
 const converterRomanianArabicNumbers = arr => {
 	for (let i = 0; i < arr.length; i++) {
 		if (typeof arr[i] === 'number') {
@@ -193,95 +107,185 @@ const converterRomanianArabicNumbers = arr => {
 };
 // Arabic to Romanian converter
 const checkArabicValue = arabicNumber => {
-	if (arabicNumber < 10 && arabicNumber > 0) {
-		getIndexOfRomanianNumber(arabicNumber);
-		return convertedNumbers.push(convertedRomanianNumber);
-	} else if (arabicNumber >= 10 && arabicNumber < 100) {
-		let twoDigitsNumber = [];
-		const units = arabicNumber % 10;
-		const tens = arabicNumber - units;
-		getIndexOfRomanianNumber(tens);
-		twoDigitsNumber.push(convertedRomanianNumber);
-		if (arabicNumber % 10 !== 0) {
-			getIndexOfRomanianNumber(units);
-			twoDigitsNumber.push(convertedRomanianNumber);
-		}
-		return convertedNumbers.push(twoDigitsNumber.join(''));
+	if (arabicNumber <= 10 && arabicNumber > 0) {
+		getUnits(arabicNumber);
+		return convertedNumbers.push(...units);
+	} else if (arabicNumber > 10 && arabicNumber < 100) {
+		const unitsNumber = arabicNumber % 10;
+		const tensNumber = (arabicNumber - unitsNumber) / 10;
+		getTens(tensNumber);
+		getUnits(unitsNumber);
+		const twoDigitsRoman = tens.concat(units);
+		return convertedNumbers.push(twoDigitsRoman.join(''));
 	} else if (arabicNumber >= 100 && arabicNumber < 1000) {
-		let threeDigitsNumber = [];
-		const units = (arabicNumber % 100) % 10;
-		const tens = (arabicNumber % 100) - units;
-		const hundreds = arabicNumber - tens - units;
-		getIndexOfRomanianNumber(hundreds);
-		threeDigitsNumber.push(convertedRomanianNumber);
-		if ((arabicNumber % 100) - units !== 0) {
-			getIndexOfRomanianNumber(tens);
-			threeDigitsNumber.push(convertedRomanianNumber);
-		}
-		if (arabicNumber % 10 !== 0) {
-			getIndexOfRomanianNumber(units);
-			threeDigitsNumber.push(convertedRomanianNumber);
-		}
-		return convertedNumbers.push(threeDigitsNumber.join(''));
+		const unitsNumber = (arabicNumber % 100) % 10;
+		const tensNumber = ((arabicNumber % 100) - unitsNumber) / 10;
+		const hundredsNumber = (arabicNumber - tensNumber - unitsNumber) / 100;
+		getHundreds(hundredsNumber);
+		getTens(tensNumber);
+		getUnits(unitsNumber);
+		const threeDigitsRoman = hundreds.concat(tens.concat(units));
+		return convertedNumbers.push(threeDigitsRoman.join(''));
 	} else if (arabicNumber >= 1000 && arabicNumber < 4000) {
-		let fourDigitsNumber = [];
-		const units = ((arabicNumber % 1000) % 100) % 10;
-		const tens = ((arabicNumber % 1000) % 100) - units;
-		const hundreds = (arabicNumber % 1000) - tens - units;
-		const thousands = arabicNumber - hundreds - tens - units;
-		getIndexOfRomanianNumber(thousands);
-		fourDigitsNumber.push(convertedRomanianNumber);
-		if ((arabicNumber % 1000) - tens - units !== 0) {
-			getIndexOfRomanianNumber(hundreds);
-			fourDigitsNumber.push(convertedRomanianNumber);
-		}
-		if ((arabicNumber % 100) - units !== 0) {
-			getIndexOfRomanianNumber(tens);
-			fourDigitsNumber.push(convertedRomanianNumber);
-		}
-		if (arabicNumber % 10 !== 0) {
-			getIndexOfRomanianNumber(units);
-			fourDigitsNumber.push(convertedRomanianNumber);
-		}
-		return convertedNumbers.push(fourDigitsNumber.join(''));
+		const unitsNumber = ((arabicNumber % 1000) % 100) % 10;
+		const tensNumber = (((arabicNumber % 1000) % 100) - unitsNumber) / 10;
+		const hundredsNumber = ((arabicNumber % 1000) - tensNumber * 10 - unitsNumber) / 100;
+		const thousandsNumber = (arabicNumber - hundredsNumber * 100 - tensNumber * 10 - unitsNumber) / 1000;
+		getThousands(thousandsNumber);
+		getHundreds(hundredsNumber);
+		getTens(tensNumber);
+		getUnits(unitsNumber);
+		const fourDigitsRoman = thousands.concat(hundreds.concat(tens.concat(units)));
+		return convertedNumbers.push(fourDigitsRoman.join(''));
 	} else return convertedNumbers.push('Poza zakresem lb. Rzymskich');
 };
 const getIndexOfRomanianNumber = arabicNumber => {
-	const digitObject = romanianNumbers.find(digitObject => digitObject.value === arabicNumber);
-	return (convertedRomanianNumber = digitObject.symbol);
+	if (romanianNumbers.find(digitObject => digitObject.value === arabicNumber)) {
+		const digitObject = romanianNumbers.find(digitObject => digitObject.value === arabicNumber);
+		return (convertedRomanianNumber = digitObject.symbol);
+	} else return false;
+};
+const getUnits = arabicNumber => {
+	units = [];
+	convertedRomanianNumber = '';
+	if (getIndexOfRomanianNumber(arabicNumber) === false) {
+		if (arabicNumber % 5 < 4 && arabicNumber < 4)
+			// Digits from 1 to 3
+			return units.push(convertedRomanianNumber.padStart(arabicNumber, romanianNumbers[0].symbol));
+		else if (arabicNumber % 5 < 4 && arabicNumber > 4)
+			// Digits from 6 to 8
+			return units.push(
+				convertedRomanianNumber
+					.padStart(1, romanianNumbers[1].symbol)
+					.padEnd(arabicNumber - 4, romanianNumbers[0].symbol)
+			);
+		else if (arabicNumber === 4)
+			return units.push(
+				convertedRomanianNumber.padStart(1, romanianNumbers[0].symbol).padEnd(2, romanianNumbers[1].symbol)
+			);
+		else if (arabicNumber === 9)
+			return units.push(
+				convertedRomanianNumber.padStart(1, romanianNumbers[0].symbol).padEnd(2, romanianNumbers[2].symbol)
+			);
+	} else {
+		units.push(getIndexOfRomanianNumber(arabicNumber));
+	}
+};
+const getTens = arabicNumber => {
+	tens = [];
+	convertedRomanianNumber = '';
+	if (getIndexOfRomanianNumber(arabicNumber * 10) === false) {
+		if (arabicNumber % 5 < 4 && arabicNumber < 4)
+			// Digits from 1 to 3
+			return tens.push(convertedRomanianNumber.padStart(arabicNumber, romanianNumbers[2].symbol));
+		else if (arabicNumber % 5 < 4 && arabicNumber > 4)
+			// Digits from 6 to 8
+			return tens.push(
+				convertedRomanianNumber
+					.padStart(1, romanianNumbers[3].symbol)
+					.padEnd(arabicNumber - 4, romanianNumbers[2].symbol)
+			);
+		else if (arabicNumber === 4)
+			return tens.push(
+				convertedRomanianNumber.padStart(1, romanianNumbers[2].symbol).padEnd(2, romanianNumbers[3].symbol)
+			);
+		else if (arabicNumber === 9)
+			return tens.push(
+				convertedRomanianNumber.padStart(1, romanianNumbers[2].symbol).padEnd(2, romanianNumbers[4].symbol)
+			);
+	} else {
+		tens.push(getIndexOfRomanianNumber(arabicNumber * 10));
+	}
+};
+const getHundreds = arabicNumber => {
+	hundreds = [];
+	convertedRomanianNumber = '';
+	if (getIndexOfRomanianNumber(arabicNumber * 100) === false) {
+		if (arabicNumber % 5 < 4 && arabicNumber < 4)
+			// Digits from 1 to 3
+			return hundreds.push(convertedRomanianNumber.padStart(arabicNumber, romanianNumbers[4].symbol));
+		else if (arabicNumber % 5 < 4 && arabicNumber > 4)
+			// Digits from 6 to 8
+			return hundreds.push(
+				convertedRomanianNumber
+					.padStart(1, romanianNumbers[5].symbol)
+					.padEnd(arabicNumber - 4, romanianNumbers[4].symbol)
+			);
+		else if (arabicNumber === 4)
+			return hundreds.push(
+				convertedRomanianNumber.padStart(1, romanianNumbers[4].symbol).padEnd(2, romanianNumbers[5].symbol)
+			);
+		else if (arabicNumber === 9)
+			return hundreds.push(
+				convertedRomanianNumber.padStart(1, romanianNumbers[4].symbol).padEnd(2, romanianNumbers[6].symbol)
+			);
+	} else {
+		hundreds.push(getIndexOfRomanianNumber(arabicNumber * 100));
+	}
+};
+const getThousands = arabicNumber => {
+	thousands = [];
+	convertedRomanianNumber = '';
+	if (getIndexOfRomanianNumber(arabicNumber * 1000) === false) {
+		if (arabicNumber % 5 < 4 && arabicNumber < 4)
+			// Digits from 1 to 3
+			return thousands.push(convertedRomanianNumber.padStart(arabicNumber, romanianNumbers[6].symbol));
+	} else {
+		thousands.push(getIndexOfRomanianNumber(arabicNumber * 1000));
+	}
 };
 // Romanian to Arabic converter
 const checkRomanianValue = romanianNumber => {
+	if (checkCorrectSymbol(romanianNumber)) {
+		checkSymbolOrder(romanianNumber);
+	} else return convertedNumbers.push('Błędny format lb. rzymskiej.');
+};
+const checkCorrectSymbol = romanianNumber => {
+	for (let i = 0; i < romanianNumber.length; i++) {
+		if (romanianNumbers.some(el => el.symbol === romanianNumber.at(i)));
+		else return false;
+	}
+	return true;
+};
+const checkSymbolOrder = romanianNumber => {
+	romanianValue = [];
+	for (let i = 0; i < romanianNumber.length; i++) {
+		const romanianNumberValue = romanianNumbers.find(el => el.symbol === romanianNumber.at(i));
+		romanianValue.push(romanianNumberValue.value);
+	}
+	for (let i = 0; i < romanianValue.length; i++) {
+		if (romanianValue[i] >= romanianValue[i + 1] || romanianValue[i + 1] === undefined) {
+		} else return checkSubstraction(romanianValue);
+	}
+	return romanianAdding(romanianValue);
+};
+const romanianAdding = romanianValue => {
 	convertedArabicNumber = 0;
-	let numberLength = romanianNumber.length;
-	for (let i = 0; i < numberLength; i++) {
-		checkSubstraction(romanianNumber);
-		// checkCorrectSymbol(romanianNumber);
-		romanianNumber = romanianNumber.replace(romanianNumber.at(0), '');
+	for (let i = 0; i < romanianValue.length; i++) {
+		const filteredValues = romanianValue.filter(value => value === romanianValue[i]);
+		if (
+			filteredValues.length < 2 &&
+			(romanianValue[i] === romanianNumbers[1].value ||
+				romanianValue[i] === romanianNumbers[3].value ||
+				romanianValue[i] === romanianNumbers[5].value)
+		) {
+			convertedArabicNumber += romanianValue[i];
+		} else if (
+			filteredValues.length <= 3 &&
+			(romanianValue[i] === romanianNumbers[0].value ||
+				romanianValue[i] === romanianNumbers[2].value ||
+				romanianValue[i] === romanianNumbers[4].value ||
+				romanianValue[i] === romanianNumbers[6].value)
+		) {
+			convertedArabicNumber += romanianValue[i];
+		} else return convertedNumbers.push('Za dużo tych samych symboli obok siebie.');
 	}
 	convertedNumbers.push(convertedArabicNumber);
 };
-const checkCorrectSymbol = romanianNumber => {
-	const convertedToArray = Array.from(romanianNumber);
-	// console.log(convertedToArray);
-	// for (let i=0; i<romanianNumbers.length; i++) {
-	// 	romanianNumbers[i].symbol.includes(convertedToArray)
-	// }
+const checkSubstraction = romanianValue => {
+	console.log('odejmowanie');
+	convertedArabicNumber = 0;
+	const filteredValues = romanianValue.filter(value => value === romanianValue[i]);
 };
 
-const checkSubstraction = romanianNumber => {
-	if (romanianNumber.at(0) === 'I' && (romanianNumber.at(1) === 'V' || romanianNumber.at(1) === 'X')) {
-		convertedArabicNumber += -1;
-	} else if (romanianNumber.at(0) === 'X' && (romanianNumber.at(1) === 'L' || romanianNumber.at(1) === 'C')) {
-		convertedArabicNumber += -10;
-	} else if (romanianNumber.at(0) === 'C' && (romanianNumber.at(1) === 'D' || romanianNumber.at(1) === 'M')) {
-		convertedArabicNumber += -100;
-	} else {
-		addingToArabicNumber(romanianNumber.at(0));
-	}
-};
-const addingToArabicNumber = romanianNumber => {
-	const symbolObject = romanianNumbers.find(symbolObject => symbolObject.symbol === romanianNumber);
-	return (convertedArabicNumber += symbolObject.value);
-};
 converterRomanianArabicNumbers(array5);
